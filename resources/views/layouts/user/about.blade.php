6dc68af6354be7935eeea1c6d3c36011b16872ce
nathan mwamba <nathanmwamba2002@gmail.com>
2021-07-21 02:33:25 -0700

    kelcommusic

--- resources/views/layouts/user/about.blade.php
+++ resources/views/layouts/user/about.blade.php
@@ -6,11 +6,11 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="My Podcast template project">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="{{ asset('styles/bootstrap-4.1.2/bootstrap.min.css') }}">
-<link href="{{ asset('plugins/font-awesome-4.7.0/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('plugins/colorbox/colorbox.css') }}" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="{{ asset('styles/about.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('styles/about_responsive.css') }}">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="plugins/colorbox/colorbox.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="styles/about.css">
+<link rel="stylesheet" type="text/css" href="styles/about_responsive.css">
 </head>
 
 <body>
@@ -49,19 +49,19 @@
        </div>
      </div>
 
-<script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
-<script src="{{ asset('styles/bootstrap-4.1.2/popper.js') }}"></script>
-<script src="{{ asset('styles/bootstrap-4.1.2/bootstrap.min.js') }}"></script>
-<script src="{{ asset('plugins/greensock/TweenMax.min.js') }}"></script>
-<script src="{{ asset('plugins/greensock/TimelineMax.min.js') }}"></script>
-<script src="{{ asset('plugins/scrollmagic/ScrollMagic.min.js') }}"></script>
-<script src="{{ asset('plugins/greensock/animation.gsap.min.js') }}"></script>
-<script src="{{ asset('plugins/greensock/ScrollToPlugin.min.js') }}"></script>
-<script src="{{ asset('plugins/colorbox/jquery.colorbox-min.js') }}"></script>
-<script src="{{ asset('plugins/easing/easing.js') }}"></script>
-<script src="{{ asset('plugins/progressbar/progressbar.min.js') }}"></script>
-<script src="{{ asset('plugins/parallax-js-master/parallax.min.js') }}"></script>
-<script src="{{ asset('js/about.js') }}"></script>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="styles/bootstrap-4.1.2/popper.js"></script>
+<script src="styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/colorbox/jquery.colorbox-min.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/progressbar/progressbar.min.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="js/about.js"></script>
 </body>
 @yield('scripts')
 </html>
