a8e9a0935a37ee9bfac121b4abf4ba80731751af
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-08-19 11:55:18 -0700

    Update MusicController.php

--- app/Http/Controllers/user/MusicController.php
+++ app/Http/Controllers/user/MusicController.php
@@ -49,7 +49,7 @@ class MusicController extends Controller
     public function wow(Request $request)
     {
         $search = $request->get('search');
-        $songs = Music::where('username', 'like', '%'. $search.'%')->paginate(2);
+        $songs = Music::where('music', 'like', '%'. $search.'%')->paginate(2);
         $galleries = Gallery::all();
         $footer = Music::orderBy('created_at', 'desc')->paginate(3);
         return view('user.music', [
