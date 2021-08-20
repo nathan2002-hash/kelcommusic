946aff9766e71744725d860fb69a433468fbf9dd
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-08-19 21:24:07 -0700

    Update mdownload.blade.php

--- resources/views/user/mdownload.blade.php
+++ resources/views/user/mdownload.blade.php
@@ -5,7 +5,7 @@
 <title>{{ $music->title }} | Kelcommusic</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="My Podcast template project">
+<meta name="description" content="Kelcom Music">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
 <link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
