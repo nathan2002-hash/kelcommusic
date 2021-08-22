434be0974818fff141faf6a99ac69c5e78f15927
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-08-22 10:54:09 -0700

    Update video.blade.php

--- resources/views/user/video.blade.php
+++ resources/views/user/video.blade.php
@@ -64,7 +64,7 @@
                 <div class="show">
                     <div class="show_image">
                         <a href="/videoshow{{ $video->id }}">
-                             <video src="{{ Storage::disk('spaces')->url('video' .$video->video) }}" width="360" alt=""></video>
+                             <video src="{{ Storage::disk('spaces')->url('video/' .$video->video) }}" width="360" alt=""></video>
                             <div class="show_play_icon"><img src="{{ asset('images/play.svg') }}" alt=""></div>
                             <div class="show_title_2">{{ $video->username }}
                             @if ($video->featuring)
