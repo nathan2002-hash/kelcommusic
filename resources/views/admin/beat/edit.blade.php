52eee4b7ca5fc74f195e305a545d9eee5a957da4
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-08-30 11:02:59 -0700

    Add files via upload

---
+++ resources/views/admin/beat/edit.blade.php
@@ -0,0 +1,203 @@
+@extends('kelcom.admin.upload')
+
+
+
+@section('title')
+    Edit {{ $beat->title }}
+@endsection
+
+
+@section('content')
+<div class="menu">
+    <div class="menu_content d-flex flex-column align-items-end justify-content-start">
+        <ul class="menu_nav_list text-right">
+            <li><a href="/adminmusiccreate">Music</a></li>
+            <li><a href="/adminbiographycreate">Biography</a></li>
+            <li><a href="/adminadvancecreate">Advance</a></li>
+            <li><a href="/adminvideocreate">Video</a></li>
+            <li><a href="/admingallery">Gallery</a></li>
+            <li><a href="/adminartistcreate">Artist</a></li>
+        </ul>
+    </div>
+</div>
+
+<!-- Home -->
+
+<div class="home">
+    <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="{{ asset('images/episode.jpg') }}" data-speed="0.8"></div>
+    <div class="home_container">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="home_content text-center">
+                        <div class="home_title"><h1>{{ $music->title }} - {{ $music->username }}
+                        @if ($music->featuring)
+                            Ft {{ $music->featuring }}
+                        @else
+
+                        @endif
+                        </h1></div>
+                        <div class="home_subtitle text-center">{{ $music->day }} {{ $music->month }}, {{ $music->year }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="home_player_container">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9 offset-lg-3">
+
+                    <!-- Episode -->
+                    <div class="player d-flex flex-row align-items-start justify-content-start s1">
+                        <div class="player_content">
+
+                            <!-- Player -->
+                            <div class="single_player_container">
+
+                                <div class="single_player d-flex flex-row align-items-center justify-content-start">
+                                    <div id="jplayer_1" class="jp-jplayer"></div>
+                                    <div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
+                                        <div class="jp-type-single">
+                                            <div class="player_controls">
+                                                <div class="jp-gui jp-interface d-flex flex-row align-items-center justify-content-start">
+                                                    <div class="jp-controls-holder time_controls d-flex flex-row align-items-center justify-content-between">
+                                                        <div>
+                                                            <div class="jp-controls-holder play_button ml-auto">
+                                                                <button class="jp-play" tabindex="0"></button>
+                                                            </div>
+                                                        </div>
+                                                        <div>
+                                                            <div class="jp-progress">
+                                                                <div class="jp-seek-bar">
+                                                                    <div class="jp-play-bar"></div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="jp-volume-controls d-flex flex-row align-items-center justify-content-between ml-auto">
+                                                        <div class="d-flex flex-row align-items-center justify-content-start">
+                                                            <button class="jp-mute" tabindex="0"></button>
+                                                        </div>
+                                                        <div class="d-flex flex-row align-items-center justify-content-start">
+                                                            <div class="jp-volume-bar">
+                                                                <div class="jp-volume-bar-value"></div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="jp-no-solution">
+                                                <span>Update Required</span>
+                                                To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="show_info d-flex flex-row align-items-start justify-content-start">
+                                <div class="show_fav d-flex flex-row align-items-center justify-content-start">
+                                    <div class="show_fav_icon show_info_icon"><img class="svg" src="images/heart.svg" alt=""></div>
+                                    <div class="show_fav_count">2371</div>
+                                </div>
+                                <div class="show_comments">
+                                    <a href="#">
+                                        <div class="d-flex flex-row align-items-center justify-content-start">
+                                            <div class="show_comments_icon show_info_icon"><img class="svg" src="images/speech-bubble.svg" alt=""></div>
+                                            <div class="show_comments_count">88 Comments</div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Episode -->
+
+<div class="episode_container">
+
+    <!-- Episode Image -->
+    <div class="episode_image_container">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <!-- Episode Image -->
+                    <div class="episode_image"><img src="images/episode_2.jpg" alt=""></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+
+            <!-- Sidebar -->
+            <div class="col-lg-3 order-lg-1 order-2 sidebar_col">
+                <div class="sidebar">
+
+                    <!-- Categories -->
+                    <div class="sidebar_list">
+                        <div class="sidebar_title">Quick Links</div>
+                        <ul>
+                            <li><a href="/adminmusic">Music</a></li>
+                            <li><a href="/adminartist">Artist</a></li>
+                            <li><a href="/adminvideo">Video</a></li>
+                            <li><a href="/adminbiography">Biography</a></li>
+                            <li><a href="/admingallery">Gallery</a></li>
+                            <li><a href="/adminadvance">Advance</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Episode -->
+            <div class="col-lg-9 episode_col order-lg-2 order-1">
+                <!-- Leave a Comment -->
+                <div class="comment_form_container">
+                    <div class="section_title">Information</div>
+                    <form action="/adminbeat{{ $beat->id }}" method="POST" enctype="multipart/form-data" class="comment_form">
+                        @csrf
+                        <div><input type="text" name="username" value="{{ $beat->studio }}" class="comment_input" placeholder="Username"></div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <input type="text" name="title" value="{{ $beat->title }}" class="comment_input" placeholder="Title" required="required">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <input type="number" name="day" value="{{ $beat->day }}" class="comment_input" placeholder="Day" required="required">
+                            </div>
+                            <div class="col-md-4">
+                                <input type="text" name="month" value="{{ $beat->month }}" class="comment_input" placeholder="Month" required="required">
+                            </div>
+                            <div class="col-md-4">
+                                <input type="number" name="year" value="{{ $beat->year }}" class="comment_input" placeholder="Year" required="required">
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <input type="text" name="image" value="{{ $beat->image }}" class="comment_input" placeholder="Image">
+                        </div>
+                        <div class="col-md-12">
+                            <input type="text" name="music" value="{{ $beat->music }}" class="comment_input" placeholder="Music">
+                        </div>
+                        <button class="comment_button button_fill">Update</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+@section('scripts')
+    //
+@endsection
---
+++ resources/views/admin/beat/index.blade.php
@@ -0,0 +1,178 @@
+@extends('kelcom.admin.music')
+
+
+
+@section('title')
+    Music
+@endsection
+
+
+@section('content')
+<div class="menu">
+    <div class="menu_content d-flex flex-column align-items-end justify-content-start">
+        <ul class="menu_nav_list text-right">
+            <li><a href="/adminmusiccreate">Music</a></li>
+            <li><a href="/adminbiographycreate">Biography</a></li>
+            <li><a href="/adminadvancecreate">Advance</a></li>
+            <li><a href="/adminvideocreate">Video</a></li>
+            <li><a href="/admingallery">Gallery</a></li>
+            <li><a href="/adminartistcreate">Artist</a></li>
+        </ul>
+    </div>
+</div>
+
+<!-- Home -->
+
+<div class="home">
+    <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="{{ asset('images/about.jpg') }}" data-speed="0.8"></div>
+    <div class="home_container d-flex flex-column align-items-center justify-content-center">
+        <div class="home_content">
+            <div class="home_title"><h1>Beats</h1></div>
+        </div>
+    </div>
+</div>
+
+<!-- Episodes -->
+
+<div class="episodes">
+    <div class="container">
+        <div class="row episodes_row">
+
+            <!-- Sidebar -->
+            <div class="col-lg-3">
+                <div class="sidebar">
+
+
+                    <!-- Categories -->
+                    <div class="sidebar_list">
+                        <div class="sidebar_title">Quick Links</div>
+                        <ul>
+                            <li><a href="/adminmusic">Music</a></li>
+                            <li><a href="/adminartist">Artist</a></li>
+                            <li><a href="/adminvideo">Video</a></li>
+                            <li><a href="/adminbiography">Biography</a></li>
+                            <li><a href="/admingallery">Gallery</a></li>
+                            <li><a href="/adminadvance">Advance</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Episodes -->
+            <div class="col-lg-9 episodes_col">
+                <div class="container-fluid">
+                    <form action="{{ route('adminmusicsearch') }}" method="POST" class="form-inline justify-content-center">
+                        {{ csrf_field() }}
+                        <div class="input-group m-2">
+                            <input type="search" class="form-control" name="name" placeholder="Search Music....">
+                        </div>
+                        <button type="submit" class="btn musica-btn m-2">Search</button>
+                    </form>
+                <br>
+                <div class="episodes_container">
+                    <?php
+                         function getDateTimeDiff($date){
+                                            $now_timestamp = strtotime(date('Y-m-d H:i:s'));
+                                            $diff_timestamp = $now_timestamp - strtotime($date);
+
+                                            if($diff_timestamp < 60){
+                                                return 'few seconds ago';
+                                            }
+                                            else if ($diff_timestamp>=60 && $diff_timestamp<3600){
+                                                return round($diff_timestamp/60).' Min(s) ago';
+                                            }
+                                            else if ($diff_timestamp>=3600 && $diff_timestamp<86400){
+                                                return round($diff_timestamp/3600).' Hour(s) ago';
+                                            }
+                                            else if ($diff_timestamp>=86400 && $diff_timestamp<(86400*7)){
+                                                return round($diff_timestamp/(86400)).' Day(s) ago';
+                                            }
+                                            else if ($diff_timestamp>=86400*7 && $diff_timestamp<(86400*30)){
+                                                return round($diff_timestamp/(86400*7)).' Week(s) ago';
+                                            }
+                                            else if ($diff_timestamp>=(86400*30) && $diff_timestamp<(86400*365)){
+                                                return round($diff_timestamp/(86400*30)).' Month(s) ago';
+                                            }
+                                            else{
+                                                return round($diff_timestamp/(86400*365)).' Years ago';
+                                            }
+                                        }
+                    foreach ($beats as $beat){
+                        ?>
+                        <!-- Episode -->
+                    <div class="episode d-flex flex-row align-items-start justify-content-start s1">
+                        <div>
+                            <div class="episode_image">
+                                @if ($beat->image)
+                                <img src="{{ Storage::disk('spaces')->url($beat->image) }}">
+                                @else
+                                <img src="{{ asset('images/episode_1.jpg') }}" alt="">
+                                @endif
+                                <div class="tags">
+                                    <ul class="d-flex flex-row align-items-start justify-content-start">
+                                        <li><a href="/music/download/{{ $beat->id }}">music</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="episode_content">
+                            <div class="episode_name"><a href="/adminmusicshow{{ $beat->id }}">{{ $beat->title }} - {{ $beat->Studio }}
+                            </a></div>
+                            <div class="episode_date"><a href="/adminmusicshow{{ $beat->id }}">{{ $beat->day }} {{ $beat->month }}, {{ $beat->year }}
+                                <?php
+                                 echo getDateTimeDiff($beat->created_at);
+                                ?>
+                            </a></div>
+                            <div class="show_info d-flex flex-row align-items-start justify-content-start">
+                            </div>
+                            <!-- Player -->
+                            <div class="single_player_container">
+
+                                <div class="single_player d-flex flex-row align-items-center justify-content-start">
+                                    <div id="jplayer_1" class="jp-jplayer"></div>
+                                    <div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
+                                        <div class="jp-type-single">
+                                            <div class="player_controls">
+                                                <audio preload="auto" controls>
+                                                    <source src="{{ asset('uploads/music/mp3/' . $beat->music) }}">
+                                                </audio>
+                                            </div>
+                                            <div class="jp-no-solution">
+                                                <span>Update Required</span>
+                                                To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="tags">
+                            <ul class="d-flex flex-row align-items-start justify-content-start">
+                                <li><a href="/adminmusicedit{{ $beat->id }}">Edit Track</a></li>
+                                <li>
+                                    <form action="{{ url('/adminmusicdelete'.$beat->id) }}" method="POST">
+                                    {{ method_field('DELETE') }}
+                                    {{ csrf_field() }}
+                                        <button type="submit" class="btn-btn">Delete</button>
+                                    </form>
+                               </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <?php
+                }
+                ?>
+                </div>
+                <br>
+                {{ $beats->links() }}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+@section('scripts')
+    //
+@endsection
