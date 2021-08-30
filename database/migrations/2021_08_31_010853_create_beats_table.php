ed7f89a403489eed27a0f8eef646b50d69e2d912
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-08-30 09:20:08 -0700

    Add files via upload

---
+++ database/migrations/2021_08_31_010853_create_beats_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beats', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->string('studio')->nullable();
+            $table->string('music')->nullable();
+            $table->string('image')->nullable();
+            $table->string('day')->nullable();
+            $table->string('month')->nullable();
+            $table->string('year')->nullable();
+            $table->bigInteger('views')->nullable();
+            $table->bigInteger('download_count')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beats');
+    }
+}
