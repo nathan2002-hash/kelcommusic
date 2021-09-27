b7224390999fba0e57ce6b6442f3ddc2c47dbdf6
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-09-26 22:58:36 -0700

    Update mdownload.blade.php

--- resources/views/user/mdownload.blade.php
+++ resources/views/user/mdownload.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>{{ $music->title }} | Kelcommusic</title>
+<title>{{ $music->title }} - {{ $music->artist->username }} | Kelcommusic</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Kelcom Music">
