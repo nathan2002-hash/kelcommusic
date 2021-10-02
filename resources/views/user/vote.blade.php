0c53fa6aa641bdae19694f51bbcc63bbe4706824
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-10-02 09:13:25 -0700

    Add files via upload

---
+++ resources/views/user/vote.blade.php
@@ -0,0 +1,113 @@
+@extends('kelcom.user.music')
+
+
+
+@section('title')
+    Votes Section
+@endsection
+
+
+@section('content')
+<div class="menu">
+    <div class="menu_content d-flex flex-column align-items-end justify-content-start">
+        <ul class="menu_nav_list text-right">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/music">Music</a></li>
+            <li><a href="/videos">videos</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+        <div class="menu_extra d-flex flex-column align-items-end justify-content-start">
+            <div class="social">
+                <ul class="d-flex flex-row align-items-start justify-content-start">
+                    <li><a href="https://web.facebook.com/kelcomcommunity"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.linkedin.com/company/kelcom-music/?viewAsMember=true"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.youtube.com/channel/UCpeko27SGsN82OLUOZ7OUtws"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Home -->
+
+<div class="home">
+    <div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="{{ asset('images/about.jpg') }}" data-speed="0.8"></div>
+    <div class="home_container d-flex flex-column align-items-center justify-content-center">
+        <div class="home_content">
+            <div class="home_title"><h1>Music</h1></div>
+        </div>
+    </div>
+</div>
+
+<!-- Episodes -->
+
+<div class="episodes">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="season_links">
+						<ul class="d-flex flex-row align-items-start justify-content-center flex-wrap">
+							<li><a href="/" class="item_filter_btn" data-filter="*">Home</a></li>
+							<li><a href="/videos" class="item_filter_btn" data-filter=".s1">Videos</a></li>
+							<li><a href="/about" class="item_filter_btn" data-filter=".s2">About</a></li>
+							<li><a href="/contact" class="item_filter_btn" data-filter=".s3">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+        <div class="row episodes_row">
+
+            <!-- Sidebar -->
+            <div class="col-lg-3">
+                <div class="sidebar">
+
+
+                    <!-- Categories -->
+                    <div class="sidebar_tags">
+                        <div class="sidebar_title">Vote For The Artist Of Your Choice</div>
+                        <div class="tags">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Episodes -->
+				@foreach ($candidates as $candidate)
+                <div class="col-lg-9 episodes_col">
+					<div class="episodes_container">
+
+						<!-- Episode -->
+						<div class="episode d-flex flex-row align-items-start justify-content-start s1">
+							<div>
+								<div class="episode_image">
+									<img src="{{ asset('/uploads/vote/' .$candidate->image) }}" alt="">
+								</div>
+							</div>
+							<div class="episode_content">
+								<div class="episode_name"><a href="episode.html">{{ $candidate->artist }}</a></div>
+								<div class="episode_date"><a href="#">Click on the button to vote for {{ $candidate->artist }}</a></div>
+								<!-- Player -->
+								<div class="single_player_container">
+
+                                    <div class="col">
+                                        <div class="button_fill shows_2_button"><a href="/clickvote{{ $candidate->id }}">Click To Vote For {{ $candidate->artist }}</a></div>
+                                    </div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+                @endforeach
+			</div>
+		</div>
+	</div>
+@endsection
+
+
+
+@section('scripts')
+    //
+@endsection
---
+++ resources/views/user/voteshow.blade.php
@@ -0,0 +1,151 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>{{ $candidate->artist }} | Kelcomcandidate</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="My Podcast template project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="plugins/colorbox/colorbox.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="styles/episode.css">
+<link rel="stylesheet" type="text/css" href="styles/episode_responsive.css">
+</head>
+<body>
+
+<div class="super_container">
+
+	<!-- Header -->
+
+	<header class="header trans_400">
+
+		<!-- Logo -->
+		<div class="logo">
+			<a href="/"><span></span>Kelcommusic</a>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="header_content d-flex flex-row align-items-center justify-content-start trans_400">
+						<nav class="main_nav">
+							<ul class="d-flex flex-row align-items-start justify-content-start">
+							<li><a href="/">Home</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/candidate">candidate</a></li>
+                            <li><a href="/videos">Videos</a></li>
+                            <li><a href="/blog">Blog</a></li>
+                            <li><a href="/contact">Contact</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Submit & Social -->
+		<div class="header_right d-flex flex-row align-items-start justify-content-start">
+
+			<!-- Social -->
+            <div class="social">
+                <ul class="d-flex flex-row align-items-start justify-content-start">
+                    <li><a href="https://web.facebook.com/kelcomcommunity"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.linkedin.com/company/kelcom-candidate/?viewAsMember=true"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.youtube.com/channel/UCpeko27SGsN82OLUOZ7OUtws"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+
+
+			<!-- Hamburger -->
+			<div class="hamburger"><i class="fa fa-bars" aria-hidden="true"></i></div>
+
+		</div>
+	</header>
+
+	<!-- Menu -->
+
+	<div class="menu">
+		<div class="menu_content d-flex flex-column align-items-end justify-content-start">
+			<ul class="menu_nav_list text-right">
+				<li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/candidate">candidate</a></li>
+                <li><a href="/videos">Videos</a></li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="/contact">Contact</a></li>
+			</ul>
+			<div class="menu_extra d-flex flex-column align-items-end justify-content-start">
+				<div class="social">
+                    <ul class="d-flex flex-row align-items-start justify-content-start">
+                        <li><a href="https://web.facebook.com/kelcomcommunity"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.linkedin.com/company/kelcom-candidate/?viewAsMember=true"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCpeko27SGsN82OLUOZ7OUtws"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+                    </ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Home -->
+
+	<div class="home">
+		<div class="parallax_background parallax-window" data-parallax="scroll" data-image-src="images/about.jpg" data-speed="0.8"></div>
+		<div class="home_container">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+                        <div class="home_content text-center">
+                            <div class="home_title"><h1>Thank you for giving a vote to {{ $candidate->artist }}
+                            </h1></div>
+                        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="home_player_container">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-9 offset-lg-3">
+                        <div class="col text-center">
+                            <div class="button_fill shows_2_button"><a href="/music">Music</a></div>
+                        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="episode_container">
+
+		<!-- Episode Image -->
+		<div class="episode_image_container">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3">
+						<!-- Episode Image -->
+						<div class="episode_image"><img src="{{ asset('uploads/vote/' .$candidate->image) }}" alt=""></div>
+					</div>
+				</div>
+			</div>
+		</div>
+        <br>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="styles/bootstrap-4.1.2/popper.js"></script>
+<script src="styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+<script src="plugins/greensock/TweenMax.min.js"></script>
+<script src="plugins/greensock/TimelineMax.min.js"></script>
+<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="plugins/greensock/animation.gsap.min.js"></script>
+<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="plugins/colorbox/jquery.colorbox-min.js"></script>
+<script src="plugins/jPlayer/jquery.jplayer.min.js"></script>
+<script src="plugins/jPlayer/jplayer.playlist.min.js"></script>
+<script src="plugins/easing/easing.js"></script>
+<script src="plugins/progressbar/progressbar.min.js"></script>
+<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
+<script src="plugins/parallax-js-master/parallax.min.js"></script>
+<script src="js/episode.js"></script>
+</body>
+</html>
