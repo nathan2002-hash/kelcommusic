8fb883e562116d560c2b4ae6c42b6105d5fc4f94
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-10-22 13:42:13 -0700

    Update MusicController.php

--- app/Http/Controllers/admin/MusicController.php
+++ app/Http/Controllers/admin/MusicController.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Storage;
 
 class MusicController extends Controller
 {
-    //public function __construct()
-    //{
-        //$this->middleware('auth:admin');
-    //}
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
 
     /**
      * Display a listing of the resource.
