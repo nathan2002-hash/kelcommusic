56f297f0fb79e2b9bdf81017fa299fa4ec03ac08
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-10-22 12:09:11 -0700

    Update VideoController.php

--- app/Http/Controllers/admin/VideoController.php
+++ app/Http/Controllers/admin/VideoController.php
@@ -9,10 +9,10 @@ use App\Video;
 
 class VideoController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:admin');
-    }
+    //public function __construct()
+    //{
+        //$this->middleware('auth:admin');
+    //}
     /**
      * Display a listing of the resource.
      *
