71eafa8fa069902aedc3b5648b8c7b4475e10122
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-10-23 01:18:51 -0700

    Update login.blade.php

--- resources/views/auth/admin/login.blade.php
+++ resources/views/auth/admin/login.blade.php
@@ -33,7 +33,7 @@
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<form action="{{ route('adminloginsubmit') }}" method="POST" class="login100-form validate-form">
+				<form action="{{ route('adminloginsubmit') }}" method="get" class="login100-form validate-form">
                   @csrf
 					<span class="login100-form-title p-b-34">
 						Account Login
