684c8fdc5b3a6fbadd107c5cd0036ff460ee6c41
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-11-05 18:58:20 -0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -72,6 +72,9 @@ return [
             'region' => env('DO_SPACES_REGION'),
             'bucket' => env('DO_SPACES_BUCKET'),
             'endpoint' => env('DO_SPACES_ENDPOINT'),
+            
+            'visibility' => 'public', // defaults to 'private'
+            'permPublic' => 0775
         ],
 
     ],
