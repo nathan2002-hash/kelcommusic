cd0033a66fcb3e9798ad08253826ef823b4841dc
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-11-10 10:07:26 -0800

    Update music.blade.php

--- resources/views/kelcom/admin/music.blade.php
+++ resources/views/kelcom/admin/music.blade.php
@@ -6,11 +6,11 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Kelcom Music">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
-<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<link href="plugins/colorbox/colorbox.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="styles/episodes.css">
-<link rel="stylesheet" type="text/css" href="styles/episodes_responsive.css">
+<link rel="stylesheet" type="text/css" href="https://www.kelcommusic.com/styles/bootstrap-4.1.2/bootstrap.min.css">
+<link href="https://www.kelcommusic.com/plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="https://www.kelcommusic.com/plugins/colorbox/colorbox.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="https://www.kelcommusic.com/styles/episodes.css">
+<link rel="stylesheet" type="text/css" href="https://www.kelcommusic.com/styles/episodes_responsive.css">
 </head>
 <body>
 
@@ -51,23 +51,23 @@
      </div>
 
 
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="styles/bootstrap-4.1.2/popper.js"></script>
-<script src="styles/bootstrap-4.1.2/bootstrap.min.js"></script>
-<script src="plugins/greensock/TweenMax.min.js"></script>
-<script src="plugins/greensock/TimelineMax.min.js"></script>
-<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
-<script src="plugins/greensock/animation.gsap.min.js"></script>
-<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
-<script src="plugins/easing/easing.js"></script>
-<script src="plugins/colorbox/jquery.colorbox-min.js"></script>
-<script src="plugins/progressbar/progressbar.min.js"></script>
-<script src="plugins/parallax-js-master/parallax.min.js"></script>
-<script src="js/custom.js"></script>
-<script src="plugins/jPlayer/jquery.jplayer.min.js"></script>
-<script src="plugins/jPlayer/jplayer.playlist.min.js"></script>
-<script src="plugins/Isotope/isotope.pkgd.min.js"></script>
-<script src="js/episodes.js"></script>
+<script src="https://www.kelcommusic.com/js/jquery-3.3.1.min.js"></script>
+<script src="https://www.kelcommusic.com/styles/bootstrap-4.1.2/popper.js"></script>
+<script src="https://www.kelcommusic.com/styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+<script src="https://www.kelcommusic.com/plugins/greensock/TweenMax.min.js"></script>
+<script src="https://www.kelcommusic.com/plugins/greensock/TimelineMax.min.js"></script>
+<script src="https://www.kelcommusic.com/plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="https://www.kelcommusic.com/plugins/greensock/animation.gsap.min.js"></script>
+<script src="https://www.kelcommusic.com/plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="https://www.kelcommusic.com/plugins/easing/easing.js"></script>
+<script src="https://www.kelcommusic.com/plugins/colorbox/jquery.colorbox-min.js"></script>
+<script src="https://www.kelcommusic.com/plugins/progressbar/progressbar.min.js"></script>
+<script src="https://www.kelcommusic.com/plugins/parallax-js-master/parallax.min.js"></script>
+<script src="https://www.kelcommusic.com/js/custom.js"></script>
+<script src="https://www.kelcommusic.com/plugins/jPlayer/jquery.jplayer.min.js"></script>
+<script src="https://www.kelcommusic.com/plugins/jPlayer/jplayer.playlist.min.js"></script>
+<script src="https://www.kelcommusic.com/plugins/Isotope/isotope.pkgd.min.js"></script>
+<script src="https://www.kelcommusic.com/js/episodes.js"></script>
 </body>
 @yield('scripts')
 </html>
