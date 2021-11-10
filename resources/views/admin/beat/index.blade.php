29231164c1b37dd9a4ed69ec688d3187619a2d70
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-11-10 09:45:38 -0800

    Update index.blade.php

--- resources/views/admin/beat/index.blade.php
+++ resources/views/admin/beat/index.blade.php
@@ -104,7 +104,7 @@
                         <div>
                             <div class="episode_image">
                                 @if ($beat->image)
-                                <img src="{{ Storage::disk('spaces')->url('bphoto/' .$beat->image) }}">
+                                <img src="{{ Storage::disk('spaces')->url($beat->image) }}">
                                 @else
                                 <img src="{{ asset('images/episode_1.jpg') }}" alt="">
                                 @endif
