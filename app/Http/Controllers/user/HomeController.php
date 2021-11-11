883b809f5d9a8c428a0a7222d2a7ad8052a1c9ff
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-11-11 05:18:54 -0800

    Add files via upload

--- app/Http/Controllers/user/HomeController.php
+++ app/Http/Controllers/user/HomeController.php
@@ -1,29 +1,63 @@
 <?php
 
-namespace App\Http\Controllers\user;
+namespace App\Http\Controllers;
 
-use App\Advance;
-use App\Gallery;
-use App\Http\Controllers\Controller;
-use App\Music;
-use App\Video;
+use App\UsersPhoneNumber;
 use Illuminate\Http\Request;
+use Twilio\Rest\Client;
+
 
 class HomeController extends Controller
 {
-    public function index()
+    public function show()
     {
-        $songs = Music::orderBy('created_at', 'desc')->paginate(3);
-        $footer = Music::orderBy('created_at', 'desc')->paginate(3);
-        $videos = Video::orderBy('created_at', 'desc')->paginate(4);
-        $galleries = Gallery::all();
-        $advances = Advance::all();
-        return view('user.home', [
-         'songs' => $songs,
-         'audios' => $footer,
-         'videos' => $videos,
-         'galleries' => $galleries,
-         'advances' => $advances
+        $users = UsersPhoneNumber::all();
+        return view('welcome', compact("users"));
+    }
+    /**
+     * Store a new user phone number.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function storePhoneNumber(Request $request)
+    {
+        //run validation on data sent in
+        $validatedData = $request->validate([
+            'phone_number' => 'required|unique:users_phone_number|numeric',
         ]);
+        $user_phone_number_model = new UsersPhoneNumber($request->all());
+        $user_phone_number_model->save();
+        $this->sendMessage('User registration successful!!', $request->phone_number);
+        return back()->with(['success' => "{$request->phone_number} registered"]);
+    }
+    /**
+     * Send message to a selected users
+     */
+    public function sendCustomMessage(Request $request)
+    {
+        $validatedData = $request->validate([
+            'users' => 'required|array',
+            'body' => 'required',
+        ]);
+        $recipients = $validatedData["users"];
+        // iterate over the array of recipients and send a twilio request for each
+        foreach ($recipients as $recipient) {
+            $this->sendMessage($validatedData["body"], $recipient);
+        }
+        return back()->with(['success' => "Messages on their way!"]);
+    }
+    /**
+     * Sends sms to user using Twilio's programmable sms client
+     * @param String $message Body of sms
+     * @param Number $recipients Number of recipient
+     */
+    private function sendMessage($message, $recipients)
+    {
+        $account_sid = getenv("TWILIO_SID");
+        $auth_token = getenv("TWILIO_AUTH_TOKEN");
+        $twilio_number = getenv("TWILIO_NUMBER");
+        $client = new Client($account_sid, $auth_token);
+        $client->messages->create($recipients, ['from' => $twilio_number, 'body' => $message]);
     }
 }
