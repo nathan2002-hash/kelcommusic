e049ca81d122f4a624ee5c9be9fd482e5d97d657
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-11-29 11:31:27 -0800

    Add files via upload

---
+++ app/Http/Controllers/admin/AlbumController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Artist;
+use App\Album;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $albums = Album::all();
+        return view('admin.album.index', [
+            'albums' => $albums
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $artists = Artist::all();
+        return view('admin.album.create', [
+            'artists' => $artists
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $album = new Album();
+        $album->album = $request->album;
+        $album->words = $request->words;
+        $album->number = $request->number;
+        $album->year = $request->year;
+        $album->artist_id = $request->artist_id;
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $extension = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $extension;
+            $file->move('uploads/albums/', $filename);
+            $album->image = $filename;
+        }
+        $album->save();
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/admin/TrackController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Album;
+use App\Artist;
+use App\Gallery;
+use App\Http\Controllers\Controller;
+use App\Music;
+use App\Track;
+use Illuminate\Http\Request;
+
+class TrackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $songs = Music::orderBy('created_at', 'desc')->paginate(50);
+        $audios = Music::orderBy('created_at', 'desc')->paginate(3);
+        $albums = Album::all();
+        $galleries = Gallery::all();
+        $artists = Artist::all();
+        return view('admin.track.create', [
+            'songs' => $songs,
+            'audios' => $audios,
+            'albums' => $albums,
+            'artists' => $artists,
+            'galleries' => $galleries
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $music = new Track();
+        $music->username = $request->username;
+        $music->video_id = $request->video_id;
+        $music->title = $request->title;
+        $music->featuring = $request->featuring;
+        $music->message = $request->message;
+        $music->artist_id = $request->artist_id;
+        $music->day = $request->day;
+        $music->month = $request->month;
+        $music->year = $request->year;
+        $music->music = $request->music;
+        if ($request-> hasfile('music')){
+            $filenamewithext = $request->file('music')->getClientOriginalName();
+            $filename = pathinfo($filenamewithext,PATHINFO_FILENAME);
+            $extension = $request->file('music')->getClientOriginalExtension();
+            $filenametostore = $filename.'_'.time().'.'.$extension;
+            $music->music = $request->music->storeAs('/track', $filenametostore, 'spaces');
+        }
+        if ($request-> hasfile('image')){
+            $filenamewithext = $request->file('image')->getClientOriginalName();
+            $filename = pathinfo($filenamewithext,PATHINFO_FILENAME);
+            $extension = $request->file('image')->getClientOriginalExtension();
+            $filenametostore = $filename.'_'.time().'.'.$extension;
+            $music->image = $request->image->storeAs('/tphoto', $filenametostore, 'spaces');
+        }
+         $music->views = $request->views;
+         $music->producer = $request->producer;
+        $music->save();
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
