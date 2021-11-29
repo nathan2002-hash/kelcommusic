6206f93bead8b36f62b56fd7022856466c42cfb3
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-11-29 12:36:52 -0800

    Update index.blade.php

--- resources/views/user/album/index.blade.php
+++ resources/views/user/album/index.blade.php
@@ -102,7 +102,7 @@
                         <div>
                             <div class="episode_image">
                                 @if ($album->image)
-                                <img src="{{ Storage::disk('spaces')->url($album->image) }}">
+                                <img src="{{ Storage::disk('spaces')->url($album->image) }}" height="200">
                                 @else
                                 <img src="{{ asset('images/episode_1.jpg') }}" alt="">
                                 @endif
