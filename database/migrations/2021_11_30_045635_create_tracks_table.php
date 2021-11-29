3fb1e5cac9cbc67128f598b23a96adebf3b1693a
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-11-29 11:19:57 -0800

    Add files via upload

---
+++ database/migrations/2021_11_30_045635_create_tracks_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('artist_id')->nullable();
+            $table->bigInteger('album_id')->nullable();
+            $table->bigInteger('video_id')->nullable();
+            $table->string('featuring')->nullable();
+            $table->string('music')->nullable();
+            $table->string('day')->nullable();
+            $table->string('month')->nullable();
+            $table->string('year')->nullable();
+            $table->longText('message')->nullable();
+            $table->string('title')->nullable();
+            $table->string('producer')->nullable();
+            $table->string('pcontact')->nullable();
+            $table->string('image')->nullable();
+            $table->string('username')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracks');
+    }
+}
