39e3933de2ce84649f57cb559263a99ff98e4c05
kelcommusic <86881052+kelcommusic@users.noreply.github.com>
2021-11-30 00:46:50 -0800

    Update mdownload.blade.php

--- resources/views/user/mdownload.blade.php
+++ resources/views/user/mdownload.blade.php
@@ -40,6 +40,7 @@
 							<li><a href="/">Home</a></li>
                             <li><a href="/about">About</a></li>
                             <li><a href="/music">Music</a></li>
+                            <li><a href="/albums">Albums</a></li>
                             <li><a href="/videos">Videos</a></li>
                             <li><a href="/blog">Blog</a></li>
                             <li><a href="/contact">Contact</a></li>
@@ -77,6 +78,7 @@
 				<li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/music">Music</a></li>
+                <li><a href="/albums">Albums</a></li>
                 <li><a href="/videos">Videos</a></li>
                 <li><a href="/blog">Blog</a></li>
                 <li><a href="/contact">Contact</a></li>
